82d8b13724d4e2bcb86802c10d32cac20a8a7e3a
jeanzless <jeanzle.albalate208@gmail.com>
2022-08-04 20:17:50 +0100

    commit

--- project-4-frontend-starter/src/App.js
+++ project-4-frontend-starter/src/App.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+
+import Home from "./components/Home"
+import Login from "./components/Login"
+import Courts from "./components/Courts"
+import Navbar from "./components/Navbar"
+import Register from "./components/Register"
+
+
+
 
 function App() {
   // ! Add your routes in here!
   return (
-    <h1>Hello world!</h1>
+    <Router>
+      <Navbar />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          
+        </Routes>
+      </main>
+    </Router>
   )
 }
 
